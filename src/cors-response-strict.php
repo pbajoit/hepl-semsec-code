96c130520392772e8ddfb13f1204e12d7ece1173
pbajoit <philippe.bajoit@gmail.com>
2026-03-31 16:37:29 +0200

    wip adapt texts

--- src/cors-response-strict.php
+++ src/cors-response-strict.php
@@ -1,9 +1,12 @@
 <?php
-include 'cors-api.php';
-
-add_headers_origin("https://" . $_SERVER['HTTP_HOST'], False, False, False);
+header('Access-Control-Allow-Origin: https://semsec.hepl-e-business.be');
+header('Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Authorization,Accept,Content-Type');
+header('Access-Control-Expose-Headers: Vary,Access-Control-Allow-Origin,Access-Control-Allow-Methods,Access-Control-Allow-Headers');
+header('Vary: Origin');
 
 header('Content-Type: application/json; charset=UTF-8');
+
 ?>
 {"response":"OK"}
 
